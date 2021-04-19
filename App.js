27349dfd70a915b7a3351889677b636d6be14280
acarerisa <acarerisa84@gmail.com>
2021-04-19 16:16:34 +0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoitem';
+import AddToDo from './components/addTodo';
+import Sandbox from './components/sandbox';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const[todos,setTodos]=useState([
+    {text:"Buy Coffee", key:'1'},
+    {text:"Create an app", key:'2'},
+    {text:"Play on the switch", key:'3'},
+  ]);
+
+  const pressHandler = (key)=>{
+    setTodos((prevTodos)=>{
+      return prevTodos.filter(todo => todo.key != key)
+    })
+  }
+
+  const submitHandler = (text)=>{
+
+    if(text !== ''){
+      setTodos((prevTodos)=>{
+        return [
+          {text:text, key:Math.random().toString() },
+          ...prevTodos
+        ];
+      })
+    }else{
+      Alert.alert('OOPPPSS!', 'You forget writing something...')
+    }
+  }
+    return (
+      //<Sandbox/>
+      <TouchableWithoutFeedback onPress={()=> {
+        Keyboard.dismiss();
+      }} >
+          <View style={styles.container}>
+            <Header/>
+          <View style={styles.content}>
+            <AddToDo submitHandler={submitHandler} />
+            <View style={styles.list}>
+                <FlatList
+                data={todos} 
+                renderItem={({ item })=>(
+                  <TodoItem item={item} pressHandler={pressHandler}/>
+                )}
+                />
+            </View>
+          </View>
+          </View>
+      </TouchableWithoutFeedback>
+    );
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content:{
+    padding:40,
+    flex:1
+  },
+  list:{
+    marginTop:20,
+    flex:1
+  }
 });
